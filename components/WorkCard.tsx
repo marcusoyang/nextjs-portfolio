0849ed0724d29e3baeaf05d55d155d1f95bd3722
Marcus O'Yang <18moyang@gmail.com>
2023-02-19 15:53:36 +1100

    added links to works

--- components/WorkCard.tsx
+++ components/WorkCard.tsx
@@ -15,56 +15,57 @@ interface WorkCardProps {
     subtitle: string;
     title: string;
     desc: string;
+    link: string;
 }
 
 const WorkCard = (props: WorkCardProps) => {
     return (
-        <>
-            <Center py={6}>
+        <a href={props.link} target='_blank' rel='noreferrer'>
+            <Box
+                my={6}
+                // border='1px red dotted'
+                w={'100%'}
+                // bg={useColorModeValue('white', '#222')}
+                boxShadow={'2xl'}
+                rounded={'sm'}
+                overflow={'hidden'}
+                p={6}
+            >
                 <Box
-                    w={'100%'}
-                    // bg={useColorModeValue('white', '#222')}
-                    boxShadow={'2xl'}
-                    rounded={'sm'}
-                    overflow={'hidden'}
-                    p={6}
+                    h={{ base: '200px', sm: '125px' }}
+                    // bg={'gray.100'}
+                    mt={-6}
+                    mx={-6}
+                    mb={6}
+                    pos={'relative'}
                 >
-                    <Box
-                        h={{ base: '200px', sm: '125px' }}
-                        // bg={'gray.100'}
-                        mt={-6}
-                        mx={-6}
-                        mb={6}
-                        pos={'relative'}
-                    >
-                        <Image
-                            layout='fill'
-                            src={props.thumbnail}
-                            alt={'Maptime'}
-                        />
-                    </Box>
-                    <Stack>
-                        <Text
-                            color={'green.500'}
-                            textTransform={'uppercase'}
-                            fontWeight={800}
-                            fontSize={'sm'}
-                            letterSpacing={1.1}
-                        >
-                            {props.subtitle}
-                        </Text>
-                        <Heading
-                            // color={useColorModeValue('gray.700', 'white')}
-                            fontSize={'2xl'}
-                            fontFamily={'body'}
-                        >
-                            {props.title}
-                        </Heading>
-                        <Text color={'gray.500'}>{props.desc}</Text>
-                    </Stack>
+                    <Image
+                        layout='fill'
+                        src={props.thumbnail}
+                        alt={'Maptime'}
+                    />
                 </Box>
-            </Center>
-        </>
+                <Stack>
+                    <Text
+                        color={'green.500'}
+                        textTransform={'uppercase'}
+                        fontWeight={800}
+                        fontSize={'sm'}
+                        letterSpacing={1.1}
+                    >
+                        {props.subtitle}
+                    </Text>
+                    <Heading
+                        // color={useColorModeValue('gray.700', 'white')}
+                        fontSize={'2xl'}
+                        fontFamily={'body'}
+                    >
+                        {props.title}
+                    </Heading>
+                    <Text color={'gray.500'}>{props.desc}</Text>
+                </Stack>
+            </Box>
+        </a>
     );
 };
 
