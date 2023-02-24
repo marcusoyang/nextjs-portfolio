0d891364aea628ff2bac798d1ec2e7cc8d98facd
Marcus O'Yang <18moyang@gmail.com>
2023-02-24 16:34:22 +1100

    updated image link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,27 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { ColorModeScript } from '@chakra-ui/react';
 
-export default class Document extends NextDocument {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
-                    <meta property='og:image' content='../public/thumb.PNG' />
-                    <meta lang='en' />
-                    <meta
-                        property='og:description'
-                        content='Portfolio website with NextJS, Chakra UI and ThreeJS'
-                    />
-                    <meta property='og:url' content='marcuso.dev' />
-                    <meta property='og:image:width' content='1200' />
-                    <meta property='og:image:height' content='627' />
-                </Head>
-                <body>
-                    {/* Make Color mode to persists when you refresh the page. */}
-                    <ColorModeScript />
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+export default function Document() {
+    return (
+        <Html lang='en'>
+            <Head>
+                {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+                <meta property='og:image' content='public/thumb.PNG' />
+                <meta property='og:title' content="Marcus O'Yang - Portfolio" />
+                <meta
+                    property='og:description'
+                    content='ReactJS portfolio website using NextJS, Chakra UI and ThreeJS'
+                />
+                <meta property='og:url' content='marcuso.dev' />
+                <meta property='og:image:width' content='1200' />
+                <meta property='og:image:height' content='627' />
+            </Head>
+            <body>
+                {/* Make Color mode to persists when you refresh the page. */}
+                <ColorModeScript />
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
